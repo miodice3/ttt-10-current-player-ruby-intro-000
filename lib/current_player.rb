153e36b01b15e04f63f50b362b105b7f2292bc3a
Michael I <miodice3@gmail.com>
2020-08-18 22:20:22 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,25 @@
+board = ["O", "X", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+    count = 0
+      board.each do |check|
+        if check == "O" #|| "X"
+          count +=1
+        elsif check == "X"
+          count +=1 
+        else
+          count = count
+        end
+    end
+end
+
+
+def current_player(count)
+  if turn_count(board) % 2 == 0
+    puts "X"
+  elsif turn_count(board) % 2 == 1
+    puts "O"
+  else
+    return
+  end
+end
